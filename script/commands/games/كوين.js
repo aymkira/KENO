d195adf844b53ac5d60c8da9d38821c0664cc864
ayman <kirabotayman@gmail.com>
2026-03-19 03:14:40 +0000

    Update كوين.js

--- script/commands/games/كوين.js
+++ script/commands/games/كوين.js
@@ -1,4 +1,5 @@
-   const path = require("path");
+const path = require("path");
+
 module.exports.config = {
     name: "كوين",
     version: "1.1.0",
@@ -12,27 +13,27 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, args }) {
     const { threadID, messageID, senderID } = event;
-    const mongodb = require(path.join(process.cwd(), "includes", "mongodb.js"));
-    
+    const db = require(path.join(process.cwd(), "includes", "data.js"));
+
     const header = `⌬ ━━━━━━━━━━━━ ⌬\n      لعبة التوقع\n⌬ ━━━━━━━━━━━━ ⌬`;
     const choice = args[0];
     const bet = parseInt(args[1]);
     const options = ["ملك", "كتابة"];
 
     if (!options.includes(choice) || isNaN(bet) || bet < 50) {
-        return api.sendMessage(`${header}\n\n⚠️ استخدم: .توقع [ملك/كتابة] [المبلغ]`, threadID, messageID);
+        return api.sendMessage(`${header}\n\n⚠️ استخدم: .كوين [ملك/كتابة] [المبلغ]`, threadID, messageID);
     }
 
-    const userData = await mongodb.getUserData(senderID);
-    if (userData.currency.money < bet) return api.sendMessage(`${header}\n\n❌ رصيدك لا يكفي للرهان!`, threadID, messageID);
+    const wallet = await db.getWallet(senderID);
+    if (wallet.money < bet) return api.sendMessage(`${header}\n\n❌ رصيدك لا يكفي للرهان!`, threadID, messageID);
 
     const result = options[Math.floor(Math.random() * options.length)];
-    
+
     if (choice === result) {
-        await mongodb.addMoney(senderID, bet);
+        await db.addMoney(senderID, bet);
         return api.sendMessage(`${header}\n\n✅ النتيجة: [ ${result} ]\n💰 ربحت: +${bet}$`, threadID, messageID);
     } else {
-        await mongodb.removeMoney(senderID, bet);
+        await db.removeMoney(senderID, bet);
         return api.sendMessage(`${header}\n\n❌ النتيجة: [ ${result} ]\n🗑️ خسرت: -${bet}$`, threadID, messageID);
     }
 };
