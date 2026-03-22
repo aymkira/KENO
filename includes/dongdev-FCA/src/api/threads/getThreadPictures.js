16fd67e3b8f2939f658717a6a44ec27b810c7091
ayman <kirabotayman@gmail.com>
2026-03-22 03:09:37 +0000

    Update getThreadPictures.js

--- includes/dongdev-FCA/src/api/threads/getThreadPictures.js
+++ includes/dongdev-FCA/src/api/threads/getThreadPictures.js
@@ -1,78 +1,43 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — Get Thread Pictures
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
 "use strict";
 
 const log = require("../../../func/logAdapter");
 const { parseAndCheckLogin } = require("../../utils/client");
+
 module.exports = function(defaultFuncs, api, ctx) {
   return function getThreadPictures(threadID, offset, limit, callback) {
-    let resolveFunc = function() {};
-    let rejectFunc = function() {};
-    const returnPromise = new Promise(function(resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
+    let resolve, reject;
+    const p = new Promise((res, rej) => { resolve = res; reject = rej; });
+    callback = callback || ((err, data) => err ? reject(err) : resolve(data));
 
-    if (!callback) {
-      callback = function(err, friendList) {
-        if (err) {
-          return rejectFunc(err);
-        }
-        resolveFunc(friendList);
-      };
-    }
-
-    let form = {
-      thread_id: threadID,
-      offset: offset,
-      limit: limit
-    };
-
-    defaultFuncs
-      .post(
-        "https://www.facebook.com/ajax/messaging/attachments/sharedphotos.php",
-        ctx.jar,
-        form
-      )
+    defaultFuncs.post(
+      "https://www.facebook.com/ajax/messaging/attachments/sharedphotos.php",
+      ctx.jar,
+      { thread_id: threadID, offset, limit }
+    )
       .then(parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.error) {
-          throw resData;
-        }
+      .then(res => {
+        if (res.error) throw res;
         return Promise.all(
-          resData.payload.imagesData.map(function(image) {
-            form = {
-              thread_id: threadID,
-              image_id: image.fbid
-            };
-            return defaultFuncs
-              .post(
-                "https://www.facebook.com/ajax/messaging/attachments/sharedphotos.php",
-                ctx.jar,
-                form
-              )
+          (res.payload?.imagesData || []).map(image =>
+            defaultFuncs.post(
+              "https://www.facebook.com/ajax/messaging/attachments/sharedphotos.php",
+              ctx.jar, { thread_id: threadID, image_id: image.fbid }
+            )
               .then(parseAndCheckLogin(ctx, defaultFuncs))
-              .then(function(resData) {
-                if (resData.error) {
-                  throw resData;
-                }
-                // the response is pretty messy
-                const queryThreadID =
-                  resData.jsmods.require[0][3][1].query_metadata.query_path[0]
-                    .message_thread;
-                const imageData =
-                  resData.jsmods.require[0][3][1].query_results[queryThreadID]
-                    .message_images.edges[0].node.image2;
-                return imageData;
-              });
-          })
+              .then(r => {
+                const qID = r.jsmods?.require?.[0]?.[3]?.[1]?.query_metadata?.query_path?.[0]?.message_thread;
+                return r.jsmods?.require?.[0]?.[3]?.[1]?.query_results?.[qID]?.message_images?.edges?.[0]?.node?.image2;
+              })
+          )
         );
       })
-      .then(function(resData) {
-        callback(null, resData);
-      })
-      .catch(function(err) {
-        log.error("Error in getThreadPictures", err);
-        callback(err);
-      });
-    return returnPromise;
+      .then(data => callback(null, data))
+      .catch(err => { log.error("getThreadPictures", err); callback(err); });
+
+    return p;
   };
 };
