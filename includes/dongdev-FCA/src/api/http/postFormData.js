d46015ae48b9a484738d66c737000f3d0307ddfc
ayman <kirabotayman@gmail.com>
2026-03-22 02:31:54 +0000

    Update postFormData.js

--- includes/dongdev-FCA/src/api/http/postFormData.js
+++ includes/dongdev-FCA/src/api/http/postFormData.js
@@ -1,47 +1,23 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — HTTP POST Form Data
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
 "use strict";
 
-var log = require("../../../func/logAdapter");
-const { getType } = require("../../utils/format");
-const { parseAndCheckLogin } = require("../../utils/client");
+const { postFormData } = require("../../utils/request");
+const { getType }      = require("../../utils/format");
+
 module.exports = function(defaultFuncs, api, ctx) {
-  return function postFormData(url, form, callback) {
-    var resolveFunc = function() {};
-    var rejectFunc = function() {};
+  return function httpPostFormData(url, form, callback) {
+    let resolve, reject;
+    const p = new Promise((res, rej) => { resolve = res; reject = rej; });
 
-    var returnPromise = new Promise(function(resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
+    callback = callback || ((err, data) => err ? reject(err) : resolve(data));
 
-    if (
-      !callback &&
-      (getType(form) == "Function" ||
-        getType(form) == "AsyncFunction")
-    ) {
-      callback = form;
-      form = {};
-    }
+    postFormData(url, ctx.jar, form || {}, {}, ctx.globalOptions, ctx)
+      .then(res => callback(null, res?.data ?? res))
+      .catch(err => callback(err));
 
-    form = form || {};
-
-    callback =
-      callback ||
-      function(err, data) {
-        if (err) return rejectFunc(err);
-        resolveFunc(data);
-      };
-
-    defaultFuncs
-      .postFormData(url, ctx.jar, form, {})
-      .then(parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        callback(null, resData);
-      })
-      .catch(function(err) {
-        log.error("postFormData", err);
-        return callback(err);
-      });
-
-    return returnPromise;
+    return p;
   };
 };
