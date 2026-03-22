c067f07aa288c00e9d05382e2d161af6e36f5639
ayman <kirabotayman@gmail.com>
2026-03-22 03:13:48 +0000

    Update markAsSeen.js

--- includes/dongdev-FCA/src/api/messaging/markAsSeen.js
+++ includes/dongdev-FCA/src/api/messaging/markAsSeen.js
@@ -1,61 +1,30 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — Mark As Seen
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
 "use strict";
 
 const log = require("../../../func/logAdapter");
 const { parseAndCheckLogin, saveCookies } = require("../../utils/client");
 const { getType } = require("../../utils/format");
+
 module.exports = function(defaultFuncs, api, ctx) {
-  return function markAsRead(seen_timestamp, callback) {
-    if (
-      getType(seen_timestamp) == "Function" ||
-      getType(seen_timestamp) == "AsyncFunction"
-    ) {
-      callback = seen_timestamp;
-      seen_timestamp = Date.now();
+  return function markAsSeen(seen_timestamp, callback) {
+    if (getType(seen_timestamp) === "Function" || getType(seen_timestamp) === "AsyncFunction") {
+      callback = seen_timestamp; seen_timestamp = Date.now();
     }
+    let resolve, reject;
+    const p = new Promise((res, rej) => { resolve = res; reject = rej; });
+    callback = callback || (err => err ? reject(err) : resolve());
 
-    let resolveFunc = function() {};
-    let rejectFunc = function() {};
-    const returnPromise = new Promise(function(resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
-
-    if (!callback) {
-      callback = function(err, friendList) {
-        if (err) {
-          return rejectFunc(err);
-        }
-        resolveFunc(friendList);
-      };
-    }
-
-    const form = {
-      seen_timestamp: seen_timestamp
-    };
-
-    defaultFuncs
-      .post(
-        "https://www.facebook.com/ajax/mercury/mark_seen.php",
-        ctx.jar,
-        form
-      )
+    defaultFuncs.post("https://www.facebook.com/ajax/mercury/mark_seen.php", ctx.jar, {
+      seen_timestamp: seen_timestamp || Date.now()
+    })
       .then(saveCookies(ctx.jar))
       .then(parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.error) {
-          throw resData;
-        }
+      .then(res => { if (res.error) throw res; callback(); })
+      .catch(err => { log.error("markAsSeen", err); callback(err); });
 
-        return callback();
-      })
-      .catch(function(err) {
-        log.error("markAsSeen", err);
-        if (getType(err) == "Object" && err.error === "Not logged in.") {
-          ctx.loggedIn = false;
-        }
-        return callback(err);
-      });
-
-    return returnPromise;
+    return p;
   };
 };
