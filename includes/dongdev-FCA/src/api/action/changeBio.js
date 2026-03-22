8614b0bc615842440d8c9a9a842030b4eb01f3c5
ayman <kirabotayman@gmail.com>
2026-03-22 02:32:45 +0000

    Update changeBio.js

--- includes/dongdev-FCA/src/api/action/changeBio.js
+++ includes/dongdev-FCA/src/api/action/changeBio.js
@@ -1,41 +1,24 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — Change Bio
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
 "use strict";
 
 const log = require("../../../func/logAdapter");
 const { parseAndCheckLogin } = require("../../utils/client");
 const { getType } = require("../../utils/format");
+
 module.exports = function(defaultFuncs, api, ctx) {
   return function changeBio(bio, publish, callback) {
-    let resolveFunc = function() {};
-    let rejectFunc = function() {};
-    const returnPromise = new Promise(function(resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
+    let resolve, reject;
+    const p = new Promise((res, rej) => { resolve = res; reject = rej; });
 
-    if (!callback) {
-      if (
-        getType(publish) == "Function" ||
-        getType(publish) == "AsyncFunction"
-      ) {
-        callback = publish;
-      } else {
-        callback = function(err) {
-          if (err) {
-            return rejectFunc(err);
-          }
-          resolveFunc();
-        };
-      }
+    if (getType(publish) === "Function" || getType(publish) === "AsyncFunction") {
+      callback = publish; publish = false;
     }
-
-    if (getType(publish) != "Boolean") {
-      publish = false;
-    }
-
-    if (getType(bio) != "String") {
-      bio = "";
-      publish = false;
-    }
+    callback = callback || (err => err ? reject(err) : resolve());
+    if (getType(publish) !== "Boolean") publish = false;
+    if (getType(bio) !== "String") { bio = ""; publish = false; }
 
     const form = {
       fb_api_caller_class: "RelayModern",
@@ -43,10 +26,10 @@ module.exports = function(defaultFuncs, api, ctx) {
       doc_id: "2725043627607610",
       variables: JSON.stringify({
         input: {
-          bio: bio,
+          bio,
           publish_bio_feed_story: publish,
           actor_id: ctx.i_userID || ctx.userID,
-          client_mutation_id: Math.round(Math.random() * 1024).toString()
+          client_mutation_id: String(Math.round(Math.random() * 1024))
         },
         hasProfileTileViewID: false,
         profileTileViewID: null,
@@ -55,21 +38,11 @@ module.exports = function(defaultFuncs, api, ctx) {
       av: ctx.i_userID || ctx.userID
     };
 
-    defaultFuncs
-      .post("https://www.facebook.com/api/graphql/", ctx.jar, form)
+    defaultFuncs.post("https://www.facebook.com/api/graphql/", ctx.jar, form)
       .then(parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.errors) {
-          throw resData;
-        }
+      .then(res => { if (res.errors) throw res; callback(); })
+      .catch(err => { log.error("changeBio", err); callback(err); });
 
-        return callback();
-      })
-      .catch(function(err) {
-        log.error("changeBio", err);
-        return callback(err);
-      });
-
-    return returnPromise;
+    return p;
   };
 };
