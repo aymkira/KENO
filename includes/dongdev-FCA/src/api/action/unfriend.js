7237b814e10f78ebe74f457ccd9e11ad63df0e50
ayman <kirabotayman@gmail.com>
2026-03-22 02:35:04 +0000

    Update unfriend.js

--- includes/dongdev-FCA/src/api/action/unfriend.js
+++ includes/dongdev-FCA/src/api/action/unfriend.js
@@ -1,54 +1,30 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — Unfriend
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
 "use strict";
 
 const log = require("../../../func/logAdapter");
 const { parseAndCheckLogin } = require("../../utils/client");
+
 module.exports = function(defaultFuncs, api, ctx) {
   return function unfriend(userID, callback) {
-    let resolveFunc = function() {};
-    let rejectFunc = function() {};
-    const returnPromise = new Promise(function(resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
-
-    if (!callback) {
-      callback = function(err, friendList) {
-        if (err) {
-          return rejectFunc(err);
-        }
-        resolveFunc(friendList);
-      };
-    }
+    let resolve, reject;
+    const p = new Promise((res, rej) => { resolve = res; reject = rej; });
+    callback = callback || (err => err ? reject(err) : resolve());
 
     const form = {
-      uid: userID,
-      unref: "bd_friends_tab",
-      floc: "friends_tab",
-      "nctr[_mod]":
-        "pagelet_timeline_app_collection_" +
-        ctx.userID +
-        ":2356318349:2"
+      uid:           userID,
+      unref:         "bd_friends_tab",
+      floc:          "friends_tab",
+      "nctr[_mod]":  `pagelet_timeline_app_collection_${ctx.userID}:2356318349:2`
     };
 
-    defaultFuncs
-      .post(
-        "https://www.facebook.com/ajax/profile/removefriendconfirm.php",
-        ctx.jar,
-        form
-      )
+    defaultFuncs.post("https://www.facebook.com/ajax/profile/removefriendconfirm.php", ctx.jar, form)
       .then(parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function(resData) {
-        if (resData.error) {
-          throw resData;
-        }
+      .then(res => { if (res.error) throw res; callback(null, true); })
+      .catch(err => { log.error("unfriend", err); callback(err); });
 
-        return callback(null, true);
-      })
-      .catch(function(err) {
-        log.error("unfriend", err);
-        return callback(err);
-      });
-
-    return returnPromise;
+    return p;
   };
 };
